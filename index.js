20202c33062bd1cbaa54d058c4ffc9dfeecb55dd
Simon Winkler <sw@fastseeders.com>
2014-12-14 19:45:09 +0100

    basic functionality with config options to store and count the steps

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 "use strict";
 
-module.exports = function fl (x, y, check, set) {
-	if (check(x, y)) {
-		fl(x + 1, y);
-		fl(x - 1, y);
-		fl(x, y + 1);
-		fl(x, y - 1);
-	}
+/**
+ * expects:
+ * config (object) (required)
+ * config.check (function) (required)
+ * config.set (function) (required)
+ * config.store_steps (bool) (optional)
+ * config.count_steps (bool) (optional)
+ *
+ * returns:
+ * (object)
+ * (object.count (number))
+ * (object.store (array))
+ */
+
+module.exports = function (config) {
+	var count = 0,
+		store = [];
+	return function fl(x, y) {
+		if (config.check(config.grid, x, y)) {
+			config.set(config.grid, x, y);
+			if (config.store_steps) {
+				store.push({x: x, y: y});
+			}
+			if (config.count_steps) {
+				count += 1;
+			}
+			fl(x + 1, y);
+			fl(x - 1, y);
+			fl(x, y + 1);
+			fl(x, y - 1);
+		}
+		return {
+			count: count,
+			store: store
+		}
+	};
 };
